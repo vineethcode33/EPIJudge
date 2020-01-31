9372b69586d34cd2a1d6114e0b69f843bc8bb8d8
vineethcode33 <vineethcode@outlook.com>
2020-01-31 15:48:52 -0800

    solved dutch_national_flag.py day1

--- epi_judge_python/int_as_array_increment.py
+++ epi_judge_python/int_as_array_increment.py
@@ -5,6 +5,16 @@ from test_framework import generic_test
 
 def plus_one(A: List[int]) -> List[int]:
     # TODO - you fill in here.
+    # A - [1, 2, 9]
+
+    carry_over = 0
+    first = True
+    for each in reversed(A):
+        each = each+carry_over
+        if first:
+            if int(each+1) == 10:
+                carry_over = 1
+
     return []
 
 
